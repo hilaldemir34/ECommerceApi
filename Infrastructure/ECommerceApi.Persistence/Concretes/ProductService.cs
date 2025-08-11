6f62d72a070d91a296f2be79b8752136cc32256f
hilaldemir34 <hilaldemir9834@gmail.com>
2025-08-11 18:23:03 +0300

    repository

---
+++ Infrastructure/ECommerceApi.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using ECommerceApi.Application.Abstactions;
+using ECommerceApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+        => new()
+        {
+            new(){Id=Guid.NewGuid(),Name="Product1",Price=100,Stock=10},
+            new(){Id=Guid.NewGuid(),Name="Product2",Price=100,Stock=10},
+            new(){Id=Guid.NewGuid(),Name="Product3",Price=100,Stock=10},
+            new(){Id=Guid.NewGuid(),Name="Product4",Price=100,Stock=10},
+            new(){Id=Guid.NewGuid(),Name="Product5",Price=100,Stock=10},
+        };
+    }
+}
